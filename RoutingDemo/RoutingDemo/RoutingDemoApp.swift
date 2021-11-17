e9a88c7d36738bfcd8399c1262a3733fea4b08e9
Nilisha gupta <nilisha.g@simformsolutions.com>
2021-11-17 18:44:23 +0530

    initial_setup

---
+++ RoutingDemo/RoutingDemo/RoutingDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoutingDemoApp.swift
+//  RoutingDemo
+//
+//  Created by Nilisha Gupta on 17/11/21.
+//
+
+import SwiftUI
+
+@main
+struct RoutingDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
